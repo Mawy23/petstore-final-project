4290c2ab906d2a705f59f3e0a130d7569b4e9fb2
Mawy23 <fgomez0723@gmail.com>
2025-01-08 16:42:50 +0100

    Faltan readmes

--- categories-service/controllers/categoriesController.js
+++ categories-service/controllers/categoriesController.js
@@ -53,12 +53,15 @@ const deleteCategory = async (req, res) => {
         const category = await Category.findById(req.params.id);
         if (!category) return res.status(404).json({ message: 'Category not found' });
 
-        await category.remove();
+        // Usamos deleteOne() para eliminar la categoría
+        await Category.findByIdAndDelete(req.params.id);
+
         res.json({ message: 'Category deleted' });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
+
 
 module.exports = {
     getCategories,
